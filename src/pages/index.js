909c3d20d57451c4932e4858385024b7f4f96fc5
Philipp Dormann <philipp.dormann1@gmail.com>
2020-08-13 13:11:29 +0200

    remove WIP state

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import classnames from 'classnames';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import styles from './styles.module.css';
+import React from "react";
+import classnames from "classnames";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./styles.module.css";
 
 const features = [
   {
@@ -12,7 +12,8 @@ const features = [
     // imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Diese Seite ist dafür gedacht, um die KonfiApp Ihrer Gemeinde schnell startklar zu machen.
+        Diese Seite ist dafür gedacht, um die KonfiApp Ihrer Gemeinde schnell
+        startklar zu machen.
       </>
     ),
   },
@@ -21,7 +22,8 @@ const features = [
     // imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
-         Hier lernen Sie die Basics der KonfiApp und finden Antworten auf einfache Fragen.
+        Hier lernen Sie die Basics der KonfiApp und finden Antworten auf
+        einfache Fragen.
       </>
     ),
   },
@@ -30,16 +32,17 @@ const features = [
     // imageUrl: 'img/undraw_docusaurus_react.svg',
     description: (
       <>
-        Falls Sie hier nicht weiterkommen, <a href="mailto:konfiapp@philippdormann.de">kontaktieren Sie mich!</a>
+        Falls Sie hier nicht weiterkommen,{" "}
+        <a href="mailto:konfiapp@philippdormann.de">kontaktieren Sie mich!</a>
       </>
     ),
   },
 ];
 
-function Feature({imageUrl, title, description}) {
+function Feature({ imageUrl, title, description }) {
   const imgUrl = useBaseUrl(imageUrl);
   return (
-    <div className={classnames('col col--4', styles.feature)}>
+    <div className={classnames("col col--4", styles.feature)}>
       {imgUrl && (
         <div className="text--center">
           <img className={styles.featureImage} src={imgUrl} alt={title} />
@@ -53,23 +56,30 @@ function Feature({imageUrl, title, description}) {
 
 function Home() {
   const context = useDocusaurusContext();
-  const {siteConfig = {}} = context;
+  const { siteConfig = {} } = context;
   return (
     <Layout
       title="Schnellstart"
-      description="Description will go into a meta tag in <head />">
-      <header className={classnames('hero hero--primary', styles.heroBanner)}>
+      description="Description will go into a meta tag in <head />"
+    >
+      <header className={classnames("hero hero--primary", styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
           <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <p><b>Hinweis: </b>Diese Seite befindet sich gerade noch im Aufbau...</p>
+          <p>
+            <b>Hinweis: </b>Wenn Sie Ideen haben, welche Informationen noch
+            fehlen:
+            <br />
+            Kontaktieren Sie mich
+          </p>
           <div className={styles.buttons}>
             <Link
               className={classnames(
-                'button button--outline button--secondary button--lg',
-                styles.getStarted,
+                "button button--outline button--secondary button--lg",
+                styles.getStarted
               )}
-              to={useBaseUrl('docs/intro')}>
+              to={useBaseUrl("docs/intro")}
+            >
               Los geht's!
             </Link>
           </div>
